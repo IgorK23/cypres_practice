6c94e04fa4044e0467a094cad5800be37c48ac6d
WIN-BRNUOG3IOSI\Home <igorcurt@gmail.com>
2022-12-14 19:53:51 +0200

    signUpTest1

---
+++ cypress/e2e/signUp.cy.js
@@ -0,0 +1,29 @@
+const { generateUser } = require('../support/generate');
+
+beforeEach(() => {
+  cy.visit('https://react-redux.realworld.io/')
+});
+
+describe('Sign Up page', () => {
+  it('should provide an ability to register new account', () => {
+    const {email, password, username} = generateUser();
+    
+    cy.get('[href="#register"]')
+      .click();
+
+    cy.get('[placeholder="Username"]')
+      .type(username);
+
+    cy.get('[placeholder="Email"]')
+      .type(email);
+
+    cy.get('[placeholder="Password"]')
+      .type(password);
+
+    cy.get('[type="submit"]')
+      .click();
+
+      cy.get(':nth-child(4) > .nav-link')
+      .should('contain', username);
+  });
+});
